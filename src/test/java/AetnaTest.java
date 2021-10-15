a28f3d9912ece32f689c996329046b79d9e6a383
MariaMoroz <morozova.mary@gmail.com>
2021-10-15 16:24:24 -0400

    add searchEmployersTest()

--- src/test/java/AetnaTest.java
+++ src/test/java/AetnaTest.java
@@ -4,17 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import Base.Base;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AetnaTest extends Base{
 
     @Test
-    public void searchEmployersTest() throws InterruptedException {
+    public void searchEmployersTest() {
 
         String expectedTextOfHeader = "Search Results";
-        Thread.sleep(3000);
+        String expextedResult =  "Employers Take on SDoH Through Analytics";
+
         SearchPage searchPage = new MainPage(getDriver())
                 .fillSearchField("Employers");
-        Thread.sleep(3000);
 
         Assert.assertEquals(searchPage.getTextHeader(), expectedTextOfHeader);
+        Assert.assertEquals(searchPage.getRowCount(), 10);
+        Assert.assertTrue(searchPage.getListResults().contains(expextedResult));
     }
 }
--- src/test/java/model/SearchPage.java
+++ src/test/java/model/SearchPage.java
@@ -4,10 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SearchPage extends MainPage{
 
     @FindBy(xpath = "//h1/span")
     private WebElement headerSearchResults;
+
+    @FindBy(xpath = "//li/h2/a")
+    private List<WebElement> listSearchResults;
 
     public SearchPage(WebDriver driver) {
         super(driver);
@@ -16,4 +22,14 @@ public class SearchPage extends MainPage{
     public String getTextHeader(){
         return headerSearchResults.getText();
     }
+
+    public List<String> getListResults(){
+        return listSearchResults.stream().map(WebElement::getText).collect(Collectors.toList());
+    }
+
+    public int getRowCount() {
+
+        return listSearchResults.size();
+    }
+
 }
